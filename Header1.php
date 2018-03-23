1a000c9deecb354e730baf7bcc06db48bb372438
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-03-23 11:53:40 +0100

    Create Header1.php

---
+++ Header1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+
+<ul>
+    <li><a class = "white" href="Formulaire%20inscription.html">Inscription</a></li>
+    <li><a class = "white" href="Connexion.html">Connexion</a></li>
+</ul>
+<h1>Wisd'home</h1>
+<image Logo="logo.jpg" />
+
+</html>
