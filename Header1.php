7c54d26de71303ee6ecce7065e75a677e71b563b
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-03-23 11:59:32 +0100

    Update Header1.php

--- Header1.php
+++ Header1.php
@@ -2,8 +2,8 @@
 <html>
 
 <ul>
-    <li><a class = "white" href="Formulaire%20inscription.html">Inscription</a></li>
-    <li><a class = "white" href="Connexion.html">Connexion</a></li>
+    <li><a class = "white" href="Inscription.php">Inscription</a></li>
+    <li><a class = "white" href="Connexion.php">Connexion</a></li>
 </ul>
 <h1>Wisd'home</h1>
 <image Logo="logo.jpg" />
