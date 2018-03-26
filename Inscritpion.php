0a4197acae9d8891e7bec5db663b29b2246e98fc
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-03-26 09:37:13 +0200

    Update and rename formulaire inscription to Inscritpion.php

---
+++ Inscritpion.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Stylesheet.css"/>
+    <link rel="stylesheet" href="Inscription.css"/>
+    <title>Inscription</title>
+</head>
+
+<header>
+    <?php include 'Header_1.php';?>
+</header>
+
+<body>
+
+<div>
+    <h1>Inscription</h1><br/>
+
+
+        <form action="" method="POST">
+            <section class = "gauche">
+                <br><label>Nom : </label><br>
+                <input type="text"><br><br>
+                <label>Prénom : </label><br>
+                <input type="text"><br><br>
+                <label>Email : </label><br>
+                <input type="text"><br><br>
+                <label>Adresse : </label><br>
+                <input type="text"><br><br>
+                <label>Code postal : </label><br>
+                <input type="text"><br><br>
+                <label>Ville : </label><br>
+                <input type="text"><br><br>
+            </section>
+            <section class = "droite">
+                <br><label>Identifiant : </label><br>
+                <input type="text"><br><br>
+                <label>Mot de passe : </label><br>
+                <input type="password"><br><br>
+                <label>Confirmation du mot de passe : </label><br>
+                <input type="password"><br><br>
+                <label>Question secrète :</label><br>
+                <select>
+                    <option>Quelle est votre ville de naissance?</option>
+                    <option>Quel est le nom de jeune fille de votre mère?</option>
+                    <option>Quel est le nom de votre premier animal de compagnie?</option>
+                </select><br><br>
+                <label>Réponse : </label><br>
+                <input type="text"><br><br>
+                <label>Acceptez-vous les conditions d'utilisation? : </label>
+                <input type="checkbox" required><br><br>
+                <input type="submit"><br>
+            </section>
+        </form>
+
+</div>
+</body>
+
+<footer>
+
+</footer>
+
+</html>
