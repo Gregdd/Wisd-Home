72f13da8e18e0b857723206bcc19dbfb6ba83354
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-03-26 09:36:06 +0200

    Create Header_2.php

---
+++ Header_2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<ul>
+    <li><a class="white" href="Accueil_1.php">Se déconnecter</a></li>
+    <li><a class = "white" href="">Gérer ma maison </a></li>
+    <li><a class = "white" href="Mon%20profil.php">Mon profil</a></li>
+</ul>
+<h1>Wisd'Home</h1>
+<image Logo="logo.jpg" />
+
+</html>
