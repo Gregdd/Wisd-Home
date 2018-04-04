ef7a285a3615a515e512f7e69a6320d083121736
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-04-04 11:14:02 +0200

    Update Header_1.php

--- Header_1.php
+++ Header_1.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html>
 
-<ul>
-    <li><a class = "white" href="Inscription.php">Inscription</a></li>
-    <li><a class = "white" href="Connexion.php">Connexion</a></li>
-</ul>
-<h1>Wisd'home</h1>
-<image Logo="logo.jpg" />
-
-</html>
