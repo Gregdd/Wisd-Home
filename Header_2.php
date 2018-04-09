3d52e9f9b3022b64413f59ad2807aefa90c1f163
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-04-09 11:31:37 +0200

    Update Header_2.php

--- Header_2.php
+++ Header_2.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="Header%202.css" />
+<div class=logo>
+  <a href="Accueil_1"><img src="logo.jpg" alt="Wisd'Home"/></a></div>
+<ol>
+    <li><p>Nom Prénom</p></li><br/>
+    <li><a class="white" href="Accueil_1.php">Deconnexion</a></li>
+</ol>
 
 <ul>
-    <li><a class="white" href="Accueil_1.php">Se déconnecter</a></li>
-    <li><a class = "white" href="">Gérer ma maison </a></li>
+    <li><a class = "white" href="Accueil_1.php">Accueil</a></li>
     <li><a class = "white" href="Mon%20profil.php">Mon profil</a></li>
+    <li><a class = "white" href="Ma maison.php">Gérer ma maison </a></li>
+    <li><a class = "white" href="Ma%20consommation.php">Ma consommation </a></li>
+    <li><a class = "white" href="Messagerie.php">Nous contacter </a></li>
+
 </ul>
-<h1>Wisd'Home</h1>
-<image Logo="logo.jpg" />
+
 
 </html>
