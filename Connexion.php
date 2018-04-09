3521126276343a3c3c8f47d9b381d7dc69a47a62
GuitaouiChaymaa <33546212+GuitaouiChaymaa@users.noreply.github.com>
2018-04-09 09:35:47 +0200

    Update Connexion.php

--- Connexion.php
+++ Connexion.php
@@ -2,13 +2,14 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <title>Connexion</title>
-    <link rel="stylesheet" href="Stylesheet.css"/>
+<meta charset="utf-8"/>
+<title>Connexion</title>
+<link rel="stylesheet" href="Stylesheet.css"/>
+<link rel="stylesheet" href="Connexion.css"/>
 </head>
 
 <header>
-    <?php include 'Header_1.php';?>
+<?php include 'Header_1.php';?>
 </header>
 
 
