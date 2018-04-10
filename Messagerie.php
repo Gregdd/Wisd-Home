9b7d551c1bb07bf30d930484ef021f88c096d043
Marvin-Nylce <32478799+Marvin-Nylce@users.noreply.github.com>
2018-04-10 09:34:14 +0200

    Add files via upload

---
+++ Messagerie.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Ma messagerie</title>
+    <link rel="stylesheet" href="Messagerie.css" />
+    <link rel="stylesheet"href="Stylesheet.css"/>
+    <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+    <script src="bootstrap-3.1.1-dist/js/bootstrap.min.js"></script>
+</head>
+
+<header>
+    <h1>Wisd'home</h1>
+    <image Logo="logo.jpg" />
+</header>
+
+<body>
+    <fieldset>
+        <legend>Contact</legend>
+        <br /><input type="text" name="objet" id="objet" placeholder="Objet" size="35"> <br /> <br/>
+        <textarea id="message" cols="100" rows="20"> >"Message pour l'administrateur..." </textarea> <br/>
+        <input type="submit" value="Envoyer"/>
+        </form>
+    </fieldset>
+
+<?php
+	$(document).ready(function() {
+   $("#message").click(function() {
+      $("p.info").remove();
+   });
+});
+?>
+</body>
+</html>
