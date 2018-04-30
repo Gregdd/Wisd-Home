ecd8923276741265decd0f8647280df21cde6b47
GuitaouiChaymaa <33546212+GuitaouiChaymaa@users.noreply.github.com>
2018-04-30 11:07:25 +0200

    Create Footer.php

---
+++ Footer.php
@@ -0,0 +1,30 @@
+<!DOCTYPE>
+<HTML>
+
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="test4.css">
+</head>
+
+<body>
+	<section>
+		<nav>
+			<div id="Conteneur">
+				<div id="element">
+					DOMISEP<br/>28 rue Notre Dame des Champs ????<br/>75006 Paris ????<br/>
+				</div>
+				<div id="element">
+					Nous suivre ?<br/><img src="logo-Fb.jpg" alt="Fb"/><img src="twitter-logo.jpg" alt="Twitter"/>
+				</div>
+				<div id="element">
+					<ul>
+						<li><a href="Mention_légales.php">Mention legales</a></li>
+						<li><a href="Conditions_d'utilisations.php">Conditions d'utilisations</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</section>
+</body>
+
+</HTML>
