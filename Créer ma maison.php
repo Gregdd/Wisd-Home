955255f42043d14225c97608a8b317865a4bb51b
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-05-02 11:18:19 +0200

    Update Créer ma maison.php

--- Créer ma maison.php
+++ Créer ma maison.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8" />
     <title>Wisd'Home - Page d'accueil</title>
-    <link rel="stylesheet" href="Stylesheet.css" />
     <link rel="stylesheet" href="Style_créer_maison.css" />
     <script src="jquery-3.2.1.js"></script>
 
