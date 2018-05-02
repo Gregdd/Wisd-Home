7aa6e61818839aa8ce034566be118020e5acf26d
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-05-02 10:46:40 +0200

    Update Header_1.php

--- Header_1.php
+++ Header_1.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html>
-
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="Header_1.css">
+</head>
+    
+<header>
+    
 <ul>
     <li><a class = "white" href="Inscription.php">Inscription</a></li>
     <li><a class = "white" href="Connexion.php">Connexion</a></li>
 </ul>
 <h1>Wisd'home</h1>
 <image Logo="logo.jpg" />
+    
+</header>
 
 </html>
