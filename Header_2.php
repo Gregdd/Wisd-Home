61fa04f415d3a0a2bf1603fb65cfbb84e97957cd
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-05-04 11:34:25 +0200

    Update Header_2.php

--- Header_2.php
+++ Header_2.php
@@ -1,28 +1,27 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="Header_2.css" />
-</head>
-    
+<meta charset="utf-8" />
+<link rel="stylesheet" href="Header_2.css" />
+
 <header>
-<ol>
-    <li><p>Nom Prénom</p></li><br/>
-    <li><a class="white" href="Accueil_1.php">Deconnexion</a></li>
-</ol>
 
-<div class=logo>
-  <a href="Accueil_2.php"><img src="Logo Wisd'Home.jpg" width="200" alt="Wisd'Home"/></a>
-</div>
+    <ol class = ol2>
+        <li class = li2>Nom Prénom</li>
+        <li class = li2><a class = a2 href="Accueil_1.php">Deconnexion</a></li>
+    </ol>
 
-<ul>
-    <li><a class = "white" href="Accueil_1.php">Accueil</a></li>
-    <li><a class = "white" href="Mon%20profil.php">Mon profil</a></li>
-    <li><a class = "white" href="Ma maison.php">Gérer ma maison </a></li>
-    <li><a class = "white" href="Ma%20consommation.php">Ma consommation </a></li>
-    <li><a class = "white" href="Messagerie.php">Nous contacter </a></li>
+    <div class=logo>
+        <a href="Accueil_2.php"><img src="images/logo.jpg" width="200" alt="Wisd'Home"/></a>
+    </div>
 
-</ul>
+    <ul class = ul2>
+        <li class = li2><a class = a2 href="Accueil_2.php">Accueil</a></li>
+        <li class = li2><a class = a2 href="Mon%20Profil.php">Mon profil</a></li>
+        <li class = li2><a class = a2 href="My%20Home.php">Gérer ma maison </a></li>
+        <li class = li2><a class = a2 href="Ma%20consommation.php">Ma consommation </a></li>
+        <li class = li2><a class = a2 href="Messagerie.php">Nous contacter </a></li>
 
+    </ul>
 </header>
+
 </html>
