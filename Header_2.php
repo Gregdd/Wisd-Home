077c0382d28ba596018162ec8c8b16f51085050a
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-05-07 09:28:29 +0200

    Update Header_2.php

--- Header_2.php
+++ Header_2.php
@@ -11,7 +11,7 @@
     </ol>
 
     <div class=logo>
-        <a href="Accueil_2.php"><img src="images/logo.jpg" width="200" alt="Wisd'Home"/></a>
+        <a href="Accueil_2.php"><img src="Logo Wisd'Home.jpg" width="200" alt="Wisd'Home"/></a>
     </div>
 
     <ul class = ul2>
