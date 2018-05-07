7c68af6e93b2655e237077b783d15284ef95c63c
GuitaouiChaymaa <33546212+GuitaouiChaymaa@users.noreply.github.com>
2018-05-07 10:11:54 +0200

    Update Footer.php

--- Footer.php
+++ Footer.php
@@ -8,23 +8,23 @@
 
 <footer>
 	<div id="Conteneur">
-		<div id="element">
+		<div id="element1">
 			DOMISEP<br/>
 			28 rue Notre Dame des Champs<br/>
 			75006 Paris<br/>
 		</div>
-		<div id="element">
+		<div id="element2">
 			Nous suivre ?<br/>
-			<img src="fb_photo.png" alt="Fb"/>
-			<img src="tw_photo.png" alt="Twitter"/>
+			<img src="images/fb_photo.png" alt="Fb"/>
+			<img src="images/tw_photo.png" alt="Twitter"/>
 		</div>
-		<div id="element">
+		<div id="element3">
 			<ul>
-				<li class="footer"><a href="Mention_légales.php">Mention legales</a></li>
+				<li class="footer"><a href="Mention_legales.php">Mention legales</a></li>
 				<li class="footer"><a href="Conditions_d'utilisations.php">Conditions d'utilisations</a></li>
 			</ul>
 		</div>
-	<div/>
+	</div>
 </footer>
 
 
