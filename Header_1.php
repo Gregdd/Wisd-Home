538c89bfdaca0c20becdc6b5bd465ba66e21dcd8
GuitaouiChaymaa <33546212+GuitaouiChaymaa@users.noreply.github.com>
2018-05-08 16:26:39 +0200

    Update Header_1.php

--- Header_1.php
+++ Header_1.php
@@ -4,19 +4,16 @@
     <meta charset="utf-8" />
     <link rel="stylesheet" href="Header_1.css">
 </head>
-
+    
 <header>
-
-    <ul class = ul1>
-        <li class = li1><a class = "white" href="Inscription.php">Inscription</a></li>
-        <li class = li1><a class = "white" href="Connexion.php">Connexion</a></li>
-    </ul>
-
-    <h1><a class = white href="Accueil_1.php">Wisd'Home</h1>
-
-    <div class=logo>
-        <a class = a1 href="Accueil_1.php"><img src="images/logo.jpg" width="200" alt="Wisd'Home"/></a>
-    </div>
-
+    
+<ul class="ulHeader1">
+    <li class="header1"><a class = "white" href="Inscription.php">Inscription</a></li>
+    <li class="header1"><a class = "white" href="Connexion.php">Connexion</a></li>
+</ul>
+<h1>Wisd'home</h1>
+<image Logo="logo.jpg" />
+    
 </header>
+
 </html>
