ad3eb603a48cf08ec27149e0e83f7a5233ced254
GuitaouiChaymaa <33546212+GuitaouiChaymaa@users.noreply.github.com>
2018-05-09 10:01:13 +0200

    Update Accueil_1.php

--- Accueil_1.php
+++ Accueil_1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title>Wisd'Home - Page d'accueil</title>
-    <link rel="stylesheet" href="Stylesheet.css"/>
+    <link rel="stylesheet" href="Accueil_1.css"/>
 </head>
 
 <body>
@@ -11,13 +11,18 @@
 <?php include 'Header_1.php'; ?>
 
 <main>
-<div>
-    <image Domotique="Domotique.jpg" />
-    <p>Bienvenue sur Wisd'Home</p>
-    <p><a class = lien href="Connexion.php">Commencer</a></p>
-    <p>Qui sommes-nous ?</p>
-    <p class="txt"> GRAND TEXTE SUR L'ENTREPRISE</p>
-    <image Entreprise="Entreprise.jpg" />
+<div id="Presentation">
+	<div class="photo">
+    	<img src="images/logo2.png" id="logo2" />
+    </div>
+    <div class="texte">
+    	<image Domotique="Domotique.jpg" />
+    	<p>Bienvenue sur Wisd'Home</p>
+    	<p><a class = lien href="Connexion.php">Commencer</a></p>
+    	<p>Qui sommes-nous ?</p>
+    	<p class="txt"> GRAND TEXTE SUR L'ENTREPRISE</p>
+    	<?php echo "Ceci est du <strong>texte</strong>"; ?>
+	</div>
 </div>
 </main>
     
