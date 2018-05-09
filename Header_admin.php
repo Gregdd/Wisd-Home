e4c601cd5f075272b1d9fd339684f0a242e3650b
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-05-09 10:49:04 +0200

    Add files via upload

---
+++ Header_admin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<link rel="stylesheet" href="Header_admin.css" />
+
+<header>
+
+    <ol class = ol2>
+        <li class = li2>Nom Prénom</li>
+        <li class = li2><a class = a2 href="Accueil_1.php">Déconnexion</a></li>
+    </ol>
+
+    <div class=logo>
+        <a href="accueil_admin.php"><img src="images/logo.jpg" width="200" alt="Wisd'Home"/></a>
+    </div>
+
+
+</header>
+
+</html>
---
+++ accueil_admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Wisd'Home - Page d'accueil</title>
+    <link rel="stylesheet" href="accueil_admin.css" />
+</head>
+
+<body>
+
+<?php include 'Header_admin.php';?>
+
+<main>
+
+    <div class="bouton">
+        <a class = a2 href="accueil_admin.php">Accueil</a>
+        <a class = a2 href=".php">Rechercher un client</a>
+        <a class = a2 href=".php">Stock capteurs</a>
+        <a class = a2 href=".php">Nouveau capteur</a>
+        <a class = a2 href=".php">Messagerie</a>
+    </div>
+
+</main>
+
+<?php include 'Footer.php';?>
+
+</body>
+
+</html>
