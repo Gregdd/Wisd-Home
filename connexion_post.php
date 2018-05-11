433ac7afcae2e674a1177694c9c69d915f6202a2
salimghaiti <34586457+salimghaiti@users.noreply.github.com>
2018-05-11 10:46:38 +0200

    Update connexion_post.php

--- connexion_post.php
+++ connexion_post.php
@@ -8,7 +8,7 @@ if(false==empty($_POST)) //Si le champ d'entrée n'est pas vide
     $post_password = $_POST['password'];
     while ($ligne = $entrees -> fetch())
     {
-        if($post_username==$ligne["Code_utilisateur"] ) //Si l'identifiant correspond
+        if($post_username==$ligne["pseudo"] ) //Si l'identifiant correspond
         {
             if($post_password==$ligne["Mot_de_passe"] || password_verify($post_password, $ligne["Mot_de_passe"])==true) //Si le mdp correspond
             {
@@ -16,8 +16,7 @@ if(false==empty($_POST)) //Si le champ d'entrée n'est pas vide
                 $_SESSION['nom'] = $ligne["Nom"];
                 $_SESSION['prenom'] = $ligne["Prenom"];
                 $_SESSION['status']="Active";
-                echo '<h1>BIENVENUE</h1>';
-                //header ('Location: View/backoffice_clients.php');
+                header ('Location: Acceuil_2.php');
                 exit();
             }
             else //Si les login et mdp ne match pas, on l'indique
