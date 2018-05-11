5b4b171a73d613a50d511bc6141e46ef28afbfc4
salimghaiti <34586457+salimghaiti@users.noreply.github.com>
2018-05-11 11:10:36 +0200

    Update inscription_post.php

--- inscription_post.php
+++ inscription_post.php
@@ -2,7 +2,7 @@
 
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=mydb', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=TABLE', 'root', 'root');
 }catch(Exception $e)
 {
     die('Erreur : '.$e->getMessage());
