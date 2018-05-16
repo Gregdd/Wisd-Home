d38c556866fdf1c565d7ad7d952789740f368b01
GuitaouiChaymaa <33546212+GuitaouiChaymaa@users.noreply.github.com>
2018-05-16 11:09:32 +0200

    Update Inscription.php

--- Inscription.php
+++ Inscription.php
@@ -5,8 +5,6 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="Inscription.css"/>
     <title>Inscription</title>
-    <link rel="text/javascript" href="vérifications.js"/>
-    <link rel="text/javascript" href="FonctionsJS.js"/>
 </head>
 
 <body>
@@ -25,8 +23,6 @@
             <input type="text" name="prenom" id="prenom"><br><br>
             <label for="identifiant">Identifiant : </label><br>
             <input type="text" name="identifiant" id="identifiant"><br><br>
-            <label for="bday">Date de naissance : </label><br>
-            <input type="date" name="bday" id="bday"><br><br>
             <label for="mail">Email : </label><br>
             <input type="text" name="mail" id="mail"><br><br>
             <label for="codepost">Code postal : </label><br>
@@ -40,19 +36,18 @@
         <section class = "droite">
             <label for="pass">Mot de passe : </label><br>
             <input type="password" name="pass" id="pass"><br><br>
-            <label for="pas2">Confirmation du mot de passe : </label><br>
-            <input type="password" name="pass2" id="pas2"><br><br>
+            <label for="pass2">Confirmation du mot de passe : </label><br>
+            <input type="password" name="pass2"><br><br>
             <label>Question secrète :</label><br>
-            <select>
+            <select name="question">
                 <option>Quelle est votre ville de naissance?</option>
                 <option>Quel est le nom de jeune fille de votre mère?</option>
                 <option>Quel est le nom de votre premier animal de compagnie?</option>
             </select><br><br>
-            <label>Réponse : </label><br>
-            <input type="text"><br><br>
+            <label for="Reponse">Réponse : </label><br>
+            <input type="text" name="Reponse"><br><br>
             <label>Accepter les conditions d'utilisation : </label>
-            <input type="checkbox" id = check required><br><br>
-            
+            <input type="checkbox" required><br><br>
             <input type="submit"><br>
         </section>
     </form>
@@ -60,8 +55,7 @@
 </div>
 
 <?php include 'Footer.php';?>
-<script src="vérifications.js"></script>
+
 </body>
 
 </html>
-
