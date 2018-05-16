923e3f1d4d1e5dad621d8beb31085d1928149af9
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-05-16 10:40:15 +0200

    Update inscription_post.php

--- inscription_post.php
+++ inscription_post.php
@@ -2,7 +2,7 @@
 
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=mydb', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=mydb', 'root', 'monmotdepasse');
 }catch(Exception $e)
 {
     die('Erreur : '.$e->getMessage());
