456c985c6b256cc2666970db30e1fa11c7030e78
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-05-18 11:03:21 +0200

    Update Accueil_admin.php

--- Accueil_admin.php
+++ Accueil_admin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title>Wisd'Home - Page d'accueil</title>
-    <link rel="stylesheet" href="accueil_admin.css" />
+    <link rel="stylesheet" href="Accueil_admin.css" />
 </head>
 
 <body>
@@ -13,8 +13,8 @@
 <main>
 
     <div class="bouton">
-        <a class = a2 href="accueil_admin.php">Accueil</a>
-        <a class = a2 href=".php">Rechercher un client</a>
+        <a class = a2 href="Accueil_admin.php">Accueil</a>
+        <a class = a2 href="Rechercher%20un%20client.php">Rechercher un client</a>
         <a class = a2 href=".php">Stock capteurs</a>
         <a class = a2 href=".php">Nouveau capteur</a>
         <a class = a2 href=".php">Messagerie</a>
@@ -27,3 +27,4 @@
 </body>
 
 </html>
+
