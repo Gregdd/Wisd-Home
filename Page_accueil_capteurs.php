bd9e964aee401a9ebe6d654163f524a457303159
salim <salimghaiti9@gmail.com>
2018-05-22 11:11:27 +0200

    modif salim

--- Page_accueil_capteurs.php
+++ Page_accueil_capteurs.php
@@ -15,8 +15,8 @@ try
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width">
     <title>Catalogue</title>
-    <link rel="stylesheet" type="text/css" href="Page_acceuil_capteur.css">
-    <link rel="stylesheet" type="text/css" href="styletableau.css">
+    <link rel="stylesheet" type="text/css" href="Page_accueil_capteur.css">
+    <link rel="stylesheet" type="text/css" href="style_tableau.css">
     <link rel="stylesheet" type="text/css" href="popupbox.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="icon" type="image/png" href="Image/icon.png" />
@@ -39,7 +39,7 @@ try
     while ($piece = $req->fetch())
     {
         echo '<li class="carre">
-            <a href="Page_acceuil_piece.php?ideal='.$piece['idpiece'].'>
+            <a href="Page_accueil_piece.php?ideal='.$piece['idpiece'].'>
             <p class="titre"> '.$piece['nom'].' </p></a>
         </li> ';
     }
