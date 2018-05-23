84cd81f2b889e1556dcedc490052b7dee39f0a6d
GuitaouiChaymaa <33546212+GuitaouiChaymaa@users.noreply.github.com>
2018-05-23 11:58:21 +0200

    Update Page_acceuil_capteurs.php

--- Page_acceuil_capteurs.php
+++ Page_acceuil_capteurs.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <?php
-
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=wisdhome', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=wisd\'home', 'root', 'root');
 }catch(Exception $e)
 {
     die('Erreur : '.$e->getMessage());
@@ -15,9 +14,8 @@ try
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width">
     <title>Catalogue</title>
-    <link rel="stylesheet" type="text/css" href="Page_acceuil_capteur.css">
+    <link rel="stylesheet" type="text/css" href="Page_accueil_capteur.css">
     <link rel="stylesheet" type="text/css" href="style_tableau.css">
-    <link rel="stylesheet" type="text/css" href="popupbox.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="icon" type="image/png" href="Image/icon.png" />
 
@@ -33,20 +31,17 @@ try
 <article>
     <h2>Choix de la pièce</h2>
     <?php
-
     $req = $bdd->query('SELECT idpiece,nom, superficie FROM pieces');
-    echo '<ul>';
+    echo '<ul class = "ul4">';
     while ($piece = $req->fetch())
     {
         echo '<li class="carre">
-            <a href="Page_accueil_piece.php?ideal='.$piece['idpiece'].'>
+            <a class="a4" href="Page_accueil_pièces.php?ideal='.$piece['idpiece'].'/>
             <p class="titre"> '.$piece['nom'].' </p></a>
         </li> ';
     }
     echo '</ul>';
-
     $req->closeCursor();
-
     ?>
 
 </article>
