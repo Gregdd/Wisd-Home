7a09ca3caeecbea632ce7f3493b750d857d220fc
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-05-25 11:49:14 +0200

    Update recherche_post.php

--- recherche_post.php
+++ recherche_post.php
@@ -1,26 +1,25 @@
 <?php
-$error_message='';
-$bdd = new PDO("mysql:host=localhost; dbname=wisdhome; charset=utf8","root","monmotdepasse");
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=wisdhome;charset=utf8','root','monmotdepasse');
+}
+catch(Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
 
 
+if (!empty($_POST['nom'])){
+    $nom = $_POST['nom'];
+    $_SESSION['client'] =$bdd->query("SELECT * FROM utilisateur WHERE nom LIKE '$nom'");
+    header('Location: Resultat.php');
+    exit();
+}
 
-$nom = $_POST['nom'];
-$pseudo = $_POST['pseudo'];
-
-if(!empty($nom) OR !(empty($pseudo))){
-    $entrees=$bdd->query("SELECT * FROM utilisateur"); //on se place dans la table admiistrateur de la BDD
-    while ($ligne = $entrees -> fetch())
-    {
-        if($nom==$ligne["nom"] ){
-            $_SESSION['client'] =$bdd->query("SELECT * FROM utilisateur WHERE nom = $nom");
-            header('Location : resultat.php');
-        }
-        if($pseudo == $ligne["pseudo"]){
-            $_SESSION['client'] =$bdd->query("SELECT * FROM utilisateur WHERE pseudo=$pseudo");
-            header('Location : resultat.php');
-        }
-
-    }
+if (!empty($_POST['pseudo'])){
+    $pseudo = $_POST['pseudo'];
+    $_SESSION['client']=$bdd->query("SELECT * FROM utilisateur WHERE pseudo LIKE '$pseudo'");
+    header('Location: Resultat.php');
+    exit();
 }
 
 ?>
