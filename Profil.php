c4351064752175674f56e40d9294409e225be63a
Marvin-Nylce <32478799+Marvin-Nylce@users.noreply.github.com>
2018-05-25 11:31:01 +0200

    Update Profil.php

--- Profil.php
+++ Profil.php
@@ -16,24 +16,24 @@
         <div class="Colonne1">
             <fieldset>
                 <legend>Votre identité</legend>
-                <p> <span class="soustitre">Informations générales</span><br/>
+                <p> <span class="soustitre">Informations générales</span><br/><br/>
 
                     <label for="nom"> Nom : </label> <?php echo $_SESSION['nom'];?> 
                     <br /><br />
                     <label for="prenom"> Prénom : </label> <?php echo $_SESSION['prenom'];?> 
                     <br /><br />
-                    <label> Date de naissance :</label> <?php echo $_SESSION['Date_naissance'];?> 
+                    <label> Date de naissance :</label> <?php echo $_SESSION['bday'];?> 
                     <br /><br />
                     <label for="mail"> Adresse mail : </label> <?php echo $_SESSION['mail'];?> 
                     <br/><br />
-                    <label for="adresse"> Adresse : </label> <?php echo $_SESSION['Adresse'];?> 
+                    <label for="Adresse"> Adresse : </label> <?php echo $_SESSION['Adresse'];?> 
                     <br/><br />
                     <label for="ville"> Ville : </label> <?php echo $_SESSION['Ville'];?> 
                     <br/><br />
-                    <label for="code postal"> Code postal : </label> <?php echo $_SESSION['Code_postal'];?> 
+                    <label for="code_postal"> Code postal : </label> <?php echo $_SESSION['Code_postal'];?> 
                     <br/><br />
-                    <a href="modif_profil.php" class= Modifier target="_blank">Modifier profil</a><br/>
-                </p>
+                    <a href="modif_profil.php" title="Modifier" target="_blank">Modifier profil</a><br/>
+					<a href="modif_mdp.php" title="Modifier mot de passe" target="_blank">Modifier mot de passe</a><br/>
             </fieldset>
         </div>
 
