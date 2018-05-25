8d1304cd693a5eaf6d573554d8ccca1165669b77
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-05-25 11:48:20 +0200

    Update Rechercher un client.php

--- Rechercher un client.php
+++ Rechercher un client.php
@@ -7,16 +7,18 @@
 
 <body>
 
-<?php include 'Header_admin2.php';?>
+<?php include 'Header_admin.php';?>
+
+<main>
 
     <h1>Rechercher un client :</h1>
-    <form method="post" action="recherche_post.php">
+    <form action="rechercher_post.php" method="post">
         <label for="nom">Nom : </label>
-        <input type="text" name="nom" id="nom"><br><br>
+        <input type="text" name="nom"><br><br>
         <label for="nom">Identifiant : </label>
-        <input type="text" name="pseudo" id="pseudo"><br><br>
+        <input type="text" name="pseudo"><br><br>
         <input type="submit" value ="Rechercher"><br>
-     </form>
+</main>
 
 <?php include 'Footer.php';?>
 
