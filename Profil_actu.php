434c350c156610176dca5aaf7098f55a6d4d3553
Marvin-Nylce <32478799+Marvin-Nylce@users.noreply.github.com>
2018-05-25 10:17:24 +0200

    Update Profil_actu.php

--- Profil_actu.php
+++ Profil_actu.php
@@ -10,7 +10,8 @@
     <script src="bootstrap-3.1.1-dist/js/bootstrap.min.js"></script>
 </head>
 <?php
-	$userid = $_SESSION['ID'];
+	
+	
 	try
 	{
 		$bdd = new PDO('mysql:host=localhost;port=8889;dbname=wisdhome;charset=utf8','root','root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); #mettre le port 8889
@@ -20,46 +21,62 @@
 		die('Erreur : ' . $e->getMessage());
 	}
 	
+	$userid = $_SESSION['ID'];
 	
 	if(isset($_POST['nom'])){
 		$newnom = $_POST['nom'];
-		$req = $bdd->prepare('UPDATE connexion SET nom=:newnom WHERE pseudo =\'userid\'');
-		$req->execute(array('newnom' => $newnom));
+		$req = $bdd->prepare('UPDATE connexion SET Nom=:newnom WHERE pseudo =:ID');
+		$req->execute(array(
+				'newnom' => $newnom,
+				'ID' => $userid
+		));
 	}
 	if(isset($_POST['prenom'])){
 		$newprenom = $_POST['prenom'];
-		$req = $bdd->prepare('UPDATE connexion SET prenom=:newprenom WHERE pseudo =\'userid\'');
-		$req->execute(array('newprenom' => $newprenom));
+		$req = $bdd->prepare('UPDATE connexion SET Prenom=:newprenom WHERE pseudo =:ID');
+		$req->execute(array(
+				'newprenom' => $newprenom,
+				'ID' => $userid
+				
+		));
 	}
 	if(isset($_POST['bday'])){
 		$newbday = $_POST['bday'];
-		$req = $bdd->prepare('UPDATE connexion SET Date_naissance=:newbday WHERE pseudo =\'userid\'');
-		$req->execute(array('newbday' => $newbday));
+		$req = $bdd->prepare('UPDATE connexion SET Date_naissance=:newbday WHERE pseudo =:ID');
+		$req->execute(array('newbday' => $newbday,
+				'ID' => $userid));
 	}
 	if(isset($_POST['mail'])){
 		$newmail = $_POST['mail'];
-		$req = $bdd->prepare('UPDATE connexion SET mail=:newmail WHERE pseudo =\'userid\'');
-		$req->execute(array('newmail' => $newmail));
+		$req = $bdd->prepare('UPDATE connexion SET mail=:newmail WHERE pseudo =:ID');
+		$req->execute(array('newmail' => $newmail,
+				'ID' => $userid));
 	}
 	if(isset($_POST['adresse'])){
 		$newadresse = $_POST['adresse'];
-		$req = $bdd->prepare('UPDATE connexion SET nom=:newadresse WHERE pseudo =\'userid\'');
-		$req->execute(array('newadresse' => $newadresse));
+		$req = $bdd->prepare('UPDATE connexion SET nom=:newadresse WHERE pseudo =:ID');
+		$req->execute(array(
+				'newadresse' => $newadresse,
+				'ID' => $userid
+		));
 	}
 	if(isset($_POST['code_postal'])){
 		$newpostal = $_POST['code_postal'];
-		$req = $bdd->prepare('UPDATE connexion SET Code_postal=:newpostal WHERE pseudo =\'userid\'');
-		$req->execute(array('newpostal' => $newpostal));
+		$req = $bdd->prepare('UPDATE connexion SET Code_postal=:newpostal WHERE pseudo =:ID');
+		$req->execute(array(
+				'newpostal' => $newpostal,
+				'ID' => $userid
+		));
 	}
 	if(isset($_POST['ville'])){
 		$newville = $_POST['ville'];
-		$req = $bdd->prepare('UPDATE connexion SET Ville=:newville WHERE pseudo =\'userid\'');
-		$req->execute(array('newville' => $newville));
+		$req = $bdd->prepare('UPDATE connexion SET Ville=:newville WHERE pseudo =:ID');
+		$req->execute(array(
+				'newville' => $newville,
+				'ID' => $userid
+		));
 	}
 	?>
 	<p>Informations mises à jour <br/> <a href = "Profil.php" title="Mon profil"> Cliquez pour revenir au profil </a>
-	
-	
+
 	</html>
-	
-	
