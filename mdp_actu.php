620a9d913a32d27e09691cf6baf3bbae6c605943
Marvin-Nylce <32478799+Marvin-Nylce@users.noreply.github.com>
2018-05-25 13:36:06 +0200

    Update mdp_actu.php

--- mdp_actu.php
+++ mdp_actu.php
@@ -11,11 +11,30 @@
     <script src="bootstrap-3.1.1-dist/js/bootstrap.min.js"></script>
 </head>
 <?php
-if(isset($_POST('amdp')) AND $_POST('amdp')==$mdp ){
-	if(isset($_POST('nmdp')) ){
-		if(isset($_POST('cmdp')) AND $_POST('cmdp')==$_POST('ndmp')){
-			$req->exec('UPDATE connexion SET mdp = nmdp WHERE id = \'userid\'');
-			echo 'Le mot de passe a bien ÈtÈ modifiÈ.';
+try
+{
+	$bdd = new PDO('mysql:host=localhost;port=8889;dbname=wisdhome;charset=utf8','root','root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); #mettre le port 8889
+}
+catch(Exception $e)
+{
+	die('Erreur : ' . $e->getMessage());
+}
+
+$userid = $_SESSION['ID'];
+
+$mdp = $bdd->prepare('SELECT Mot_de_passe FROM connexion WHERE pseudo = ?') or die(print_r($bdd->errorInfo()));
+$mdp->execute(array($userid));
+
+if(!empty($_POST('amdp')) AND $_POST('amdp')==$mdp ){
+	if(!empty($_POST('nmdp')) ){
+		if(!empty($_POST('cmdp')) AND $_POST('cmdp')==$_POST('ndmp')){
+			$newmdp = $_POST('ndmp');
+			$req= $bdd -> prepare('UPDATE connexion SET Mot_de_passe =:newmdp WHERE pseudo = :ID');
+			$req->execute(array(
+					'newmdp' => $newmdp,
+					'ID' => $userid
+			));
+			echo 'Le mot de passe a bien √©t√© modifi√©.';
 		}
 		else{
 			echo 'Erreur : Les mots de passe ne correspondent pas.';
