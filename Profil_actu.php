42b72feaf7079aef07e91eb6ec0a6a6ac69d0487
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-05-30 10:04:23 +0200

    Update Profil_actu.php

--- Profil_actu.php
+++ Profil_actu.php
@@ -11,15 +11,7 @@
 </head>
 <?php
 	
-	
-	try
-	{
-		$bdd = new PDO('mysql:host=localhost;port=8889;dbname=wisdhome;charset=utf8','root','root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); #mettre le port 8889
-	}
-	catch(Exception $e)
-	{
-		die('Erreur : ' . $e->getMessage());
-	}
+	include 'database.php';
 	
 	$userid = $_SESSION['ID'];
 	
