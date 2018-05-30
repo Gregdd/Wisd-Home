8afa12df3e58ed3cc336b395af5a0024ce8abb1b
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-05-30 10:12:09 +0200

    Create Mdp_oublie.php

---
+++ Mdp_oublie.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8"/>
+<title>Modification du mot de passe</title>
+<link rel="stylesheet" href="Mdp_oublie.css"/>
+<script type="text/javascript" src="Mdp_oublie.js"></script>
+</head>
+
+<body>
+
+<?php include 'Header_1.php';?>
+
+<main>
+
+<div>
+    <fieldset>
+        <legend>Modification du mot de passe</legend><br/>
+        <p>Nous allons vous envoyer un mail pour que vous puissiez récupérer votre compte en changeant votre mot de passe.</p><br/>
+        <label for="mail">Veuillez entrer votre adresse mail :</label><br/><br/>
+        <div id="failmail">Adresse mail invalide !</div><br/>
+        <input type="text" id="mail"/><br/><br/>
+        <button onclick="test(mail)" id="Envoyer">Envoyer</button>
+
+    </fieldset>
+</div>
+
+</main>
+
+    <?php include 'Footer.php';?>
+
+</body>
+
+</html>
+
