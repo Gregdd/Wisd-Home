b1eb67a7c37e0157c92343f551d01e00ca8b4395
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-05-30 10:10:39 +0200

    Create Envoi_mail.php

---
+++ Envoi_mail.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8"/>
+<title>Modification du mot de passe</title>
+<link rel="stylesheet" href="Envoi_mail.css"/>
+</head>
+
+<body>
+
+<?php include 'Header_1.php';?>
+
+<main>
+    <img src="Enveloppe.png" id="enveloppe"/>
+
+<div>
+    <fieldset>
+
+        <p>Nous avons bien pris en compte votre requête, un mail vous a été evoyé pour modifier votre mot de passe.</p>
+
+    </fieldset>
+</div>
+    </main>
+
+    <?php include 'Footer.php';?>
+
+</body>
+
+</html>
+
