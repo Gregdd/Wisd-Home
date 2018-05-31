fb33b5b26bd05979647b47b11baf6104c879e2bd
salim <salimghaiti9@gmail.com>
2018-05-31 23:25:00 +0200

    modif salim

--- Page_accueil_piece_capteur.php
+++ Page_accueil_piece_capteur.php
@@ -30,75 +30,54 @@ try
 <article>
 
     <?php
-    $id1 = $_GET["ideal1"];
+    $id2 = $_GET["ideal2"];
 
-    $reqtype = $bdd->query("SELECT typeactionneur FROM actionneurpiece WHERE ID='$id1' ");
+    $reqtype = $bdd->query("SELECT typecapteur FROM capteurpiece WHERE ID='$id2' ");
     $type = $reqtype->fetch();
-    $type1=$type['typeactionneur'];
+    $type1=$type['typecapteur'];
     $reqtype->closeCursor();
 
-    $reqnom = $bdd->query("SELECT  p.nom  FROM pieces p INNER JOIN actionneurpiece j ON j.idpiece = p.idpiece WHERE j.ID='$id1' ");
+    $reqnom = $bdd->query("SELECT  p.nom  FROM pieces p INNER JOIN capteurpiece j ON j.idpiece = p.idpiece WHERE j.ID='$id2' ");
     $nom = $reqnom->fetch();
     $nom1=$nom['nom'];
 
-    $reqval = $bdd->query("SELECT valeur FROM actionneurpiece WHERE ID='$id1' ");
+    $reqval = $bdd->query("SELECT valeur FROM capteurpiece WHERE ID='$id2' ");
     $val = $reqval->fetch();
     $val1=$val['valeur'];
     $reqval->closeCursor();
 
-    $requnit = $bdd->query("SELECT  p.unit  FROM actionneur p INNER JOIN actionneurpiece j ON j.typeactionneur = p.actionneurtype WHERE j.ID='$id1' ");
+    $requnit = $bdd->query("SELECT  p.unit  FROM capteur p INNER JOIN capteurpiece j ON j.typecapteur = p.capteurtype WHERE j.ID='$id2' ");
     $unit = $requnit->fetch();
     $unit1=$unit['unit'];
 
-    //$valeursouhaitee=$_POST['valeursouhaitee'];
-
-    $req1 = $bdd->prepare("SELECT url_img FROM actionneur WHERE actionneurtype =?");
+    $req1 = $bdd->prepare("SELECT url_img FROM capteur WHERE capteurtype =?");
     $req1 -> execute(array($type1));
     $image = $req1->fetch();
     $url_img = $image["url_img"];
 
-    if(!empty($_POST['valeursouhaitee'])) {
-        $reqval1 = $bdd->prepare('UPDATE  actionneurpiece SET valeur= ?  WHERE ID = ?');
-        $reqval1 -> execute(array($_POST['valeursouhaitee'], $id1));
-        $reqval1->closeCursor();
-        header('location:Page_acceuil_actionneur.php');
-    }
+
 
 
 
     ?>
 
-    <h2 style="text-align: center">  Actionneur: <?php echo"$type1"?><?php echo "<br>"?>     Pièce: <?php echo"$nom1"?> </h2><hr>
+    <h2 style="text-align: center">  Capteur: <?php echo$type1 ?><?php echo "<br>"?>     Pièce: <?php echo$nom1?> </h2><hr>
     <br>
     <div >
-
-        <img  src="<?php echo$url_img ?>" style=" width:100px;height:100px;; display: inline-block; margin-right: 300px; margin-left: 100px; margin-bottom: 20px"  >
+        <img  src="<?php echo $url_img ?>" style=" width:100px;height:100px;; display: inline-block; margin-right: 300px; margin-left: 100px; margin-bottom: 20px"  >
         <h3 style=" display: inline-block; margin-right: 100px"><?php echo"Valeur actuelle   :" ?></h3>
 
-        <h3 style=" display: inline-block; margin-right: 100px"><?php echo"$val1 " ;echo"    "; echo"($unit1)"?></h3>
+        <h3 style=" display: inline-block; margin-right: 100px"><?php echo"$val1 " ; echo"$unit1"?></h3>
 
-        <form name="formulaire" onsubmit="return Test()" method="post" action="" style="display: inline-block">
-            <input style=" color:black; margin-right: 100px ; background-color: #5b9630" type="text" name="valeursouhaitee" placeholder="Valeur souhaitée" />
-            <input style="background-color: #4CAF50" type="submit" value="envoyer">
-        </form>
-    </div>
+
+
+
+
 
 
 
 </article>
-<script>
-    function Test(){
-        var choix=formulaire.valeursouhaitee.value;
-        if (!choix){
-            alert(" Veuillez indiquer la valeur souhaitée !!");
-            return false;
-            formulaire.choix.focus();
-        }
 
-
-
-    }
-</script>
 
 
 
