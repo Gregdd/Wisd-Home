c7989ce03d28ea26e3ba927bfb2e94d13580ea35
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-06-01 10:15:52 +0200

    Update Mdp_oublie-post.php

--- Mdp_oublie-post.php
+++ Mdp_oublie-post.php
@@ -1,12 +1,12 @@
 <?php
 include 'database.php';
 
-$mail=$_POST['mail'];
 $req = $bdd->query('SELECT * FROM connexion WHERE mail LIKE $mail');
+$mail=$_POST['mail'];
 
   if (isset($mail)) {
         $header="MIME-Version: 1.0\r\n";
-        $header.='From:"gmail.com"<support@gmail.com>'."\n";
+        $header.='From:"Wisdhome"<contact.wisdhome@gmail.com>'."\n";
         $header.='Content-Type:text/html; charset=utf-8'."\n";
         $header.='Content-Transfer-Encoding: 8bit';
 
@@ -21,9 +21,9 @@ $req = $bdd->query('SELECT * FROM connexion WHERE mail LIKE $mail');
 </html>
 
 ';
-        mail($_POST['mail'], "Changement de mot de passe", $message, $header);
+        mail("contact.wisdhome@gmail.com", "Changement de mot de passe", $message, $header);
         $reponse = 'le mail a été envoyé !';
-    }
 
     return $reponse;
+  }
 ?>
