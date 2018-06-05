a16ec85e694d22e9bcd9a50f9a1695b2d760e285
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-06-05 09:15:06 +0200

    Update Page_accueil_capteurs.php

--- Page_accueil_capteurs.php
+++ Page_accueil_capteurs.php
@@ -1,13 +1,7 @@
 <!DOCTYPE html>
 <?php
 
-try
-{
-    $bdd = new PDO('mysql:host=localhost;dbname=wisdhome', 'root', 'root');
-}catch(Exception $e)
-{
-    die('Erreur : '.$e->getMessage());
-}
+include 'database.php';
 ?>
 
 <html>
