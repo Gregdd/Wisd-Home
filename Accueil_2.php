74a62cfa29f439321b77e080d69c9aa266cd681c
GuitaouiChaymaa <33546212+GuitaouiChaymaa@users.noreply.github.com>
2018-06-05 09:03:29 +0200

    Update Accueil_2.php

--- Accueil_2.php
+++ Accueil_2.php
@@ -1,27 +1,91 @@
 <!DOCTYPE html>
+<?php session_start();?>
 <html>
 <head>
     <meta charset="ISO-8859-1" />
     <title>Wisd'Home - Page d'accueil</title>
-    <link rel="stylesheet" href="Accueil_2.css" />
+    <link rel="stylesheet" href="Accueil_2_bis.css" />
 </head>
 
 <body>
-
+<header>
 <?php include 'Header_2.php';?>
-    
-    <main>
+</header>
+
+  
+        <h1>Bienvenue <?php echo $_SESSION['prenom']?> !</h1>
         
-        <div class="bouton">
-            <a href="Page_accueil_capteurs.php">My home</a>
-
-            <a href="Ma_consommation.php">Ma consommation</a>
-
-            <a href="Creer_ma_maison.php">Gerer ma maison</a>
-
+        <div id="Conteneur">
+        <div class="etatCapt">
+        <h2>Etat des capteurs :</h2>
+        	<form action="etat_capteurs.php" method="post">
+        		<label for="temp">Temprérature de la maison : </label><?php echo $_POST["température"]?> <br/>
+        		<label for="humid">Humidité : </label><?php echo $_POST["humidité"]?><br/>
+        		<label for="lumino">Nombre de capteurs luminosité activé(s) :</label><?php echo $_POST["nombreLuminosité"]?><br/>
+        		<label for="camera">Caméra :</label><?php echo $_POST["etatCamera"]?><br/>
+        		<label for="presence">Détecteur de présence :</label><?php echo $_POST["etatPresence"]?><br/>
+        	</form>
         </div>
         
-    </main>
+        <div class="etatCapt">
+        	<h4>Tout eteindre/allumer luminosité : </h4>
+        	<p class="onoffswitch">
+            <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
+            <label class="onoffswitch-label" for="myonoffswitch">
+                <span class="onoffswitch-inner"></span>
+                <span class="onoffswitch-switch"></span>
+            </label>
+        	</p>
+        	
+        	<h4>Tout eteindre/allumer chauffage : </h4>
+            <p class="onoffswitch">
+            <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
+            <label class="onoffswitch-label" for="myonoffswitch">
+                <span class="onoffswitch-inner"></span>
+                <span class="onoffswitch-switch"></span>
+            </label>
+        	</p>
+        	
+        	<h4>Tout ouvrir/fermer volets : </h4>
+            <p class="onoffswitch">
+            <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
+            <label class="onoffswitch-label" for="myonoffswitch">
+                <span class="onoffswitch-inner"></span>
+                <span class="onoffswitch-switch"></span>
+            </label>
+        	</p>
+        	</div>
+        	
+        	<div class="etatCapt">
+        	<h4>Tout ouvrir/fermer volets : </h4>
+            <p class="onoffswitch">
+            <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
+            <label class="onoffswitch-label" for="myonoffswitch">
+                <span class="onoffswitch-inner"></span>
+                <span class="onoffswitch-switch"></span>
+            </label>
+        	</p>
+        	
+        	<h4>Eteindre/allumer caméra : </h4>
+            <p class="onoffswitch">
+            <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
+            <label class="onoffswitch-label" for="myonoffswitch">
+                <span class="onoffswitch-inner"></span>
+                <span class="onoffswitch-switch"></span>
+            </label>
+        	</p>
+        	
+        	<h4>Eteindre/allumer présence : </h4>
+            <p class="onoffswitch">
+            <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
+            <label class="onoffswitch-label" for="myonoffswitch">
+                <span class="onoffswitch-inner"></span>
+                <span class="onoffswitch-switch"></span>
+            </label>
+        	</p>
+        </div>
+        </div>
+        
     
 <?php include 'Footer.php';?>
     
