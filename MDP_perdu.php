6df0bac7db42da02f9a0a1fbc423566c0d47bb46
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-06-06 11:33:33 +0200

    Update MDP_perdu.php

--- MDP_perdu.php
+++ MDP_perdu.php
@@ -5,6 +5,7 @@
 <meta charset="utf-8"/>
 <title>Modification du mot de passe</title>
 <link rel="stylesheet" href="MDP_perdu.css"/>
+<?php session_start();?>
 </head>
 
 <body>
@@ -16,15 +17,10 @@
     <fieldset>
         <legend>Modification du mot de passe</legend>
 
-        <form method="post" action="MDP_perdu-post.php">
+        <form  action="MDP_perdu-post.php" method="post">
             <br/>
-            <label for="Identifiant">Identifiant :</label><br/>
-            <input type="text" name="Identifiant" id="Identifiant" size="30" placeholder="Ex : Dupont Marc" maxlength="30" /><br/><br/>
-            <label for="pass">Nouveau mot de passe :</label>
-            <input type="password" name="pass" id="pass" size="30" maxlength="30" /><br/><br/>
-            <label for="pass">Confirmation du nouveau mot de passe :</label>
-            <input type="password" name="pass" size="30" maxlength="30" /><br/><br/>
-            <input type="submit" value="Valider"> <a href="Accueil_2.php" title="Page d'accueil"></a><br/><br/>
+            <label for="pseudo">Identifiant :</label><br/>
+            <input type="text" name="pseudo" id="pseudo" size="30" placeholder="Ex : Dupont Marc" maxlength="30" /><br/><br/>
         </form>
 
     </fieldset>
@@ -32,7 +28,7 @@
     </main>
 
     <?php include 'Footer.php';?>
-    
+
 </body>
 
 </html>
