c594cd75bddab26e0de0e623bad867c795987103
Marvin-Nylce <32478799+Marvin-Nylce@users.noreply.github.com>
2018-06-08 10:54:02 +0200

    Update Messages.php

--- Messages.php
+++ Messages.php
@@ -1,23 +1,19 @@
 <?php
 session_start();
-$objet = $_POST['objet'];
-$message = $_POST['message'];
-$id = $_SESSION['ID'];
-$date = date("Y-m-d H:i:s");
-
 include 'database.php';
 
-if(!empty($objet) && !empty($message)){
-	$req = $bdd->prepare('INSERT INTO Messagerie(Objet, ID, Message, Date_Message) VALUES(:objet, :iD; :message, :date_envoi)');
-	$req->execute(array(
-			'objet'=> $objet,
-			'iD' => $id,
-			'message' => $message,
-			'date_envoi' => $date		
-	));
-}
+$objet = $_POST['objet'];
+$message = $_POST['message'];
+//$id = $_SESSION['ID'];
+$date = date("Y-m-d H:i:s");
 
-echo  "Message envoyé";
-header ('Location: Accueil_2.php');
-exit();
+if(!empty($objet) && !empty($message)){
+	$req = $bdd->prepare('INSERT INTO messagerie(Objet, ID, Message, Date_Message) VALUES(?,?,?,?)') or die(print_r($bdd->errorInfo()));
+	$req->execute(array($objet,$_SESSION['ID'],$message,$date));
+	echo 'Le message a été envoyé.';
+	
+}
+else{
+	echo 'Veuillez entrer les informations !';
+}
 ?>
