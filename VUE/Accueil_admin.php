3d0d8cdb1bc1764645f4d648bc5f7f91bc521ee0
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-06-08 14:37:11 +0200

    Update and rename Accueil_admin.php to VUE/Accueil_admin.php

---
+++ VUE/Accueil_admin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Wisd'Home - Page d'accueil</title>
+    <link rel="stylesheet" href="/Wisd-Home-master/VUE/css/Accueil_admin.css" />
+</head>
+
+<body>
+
+<?php include '../VUE/Header_admin.php';?>
+
+<main>
+
+    <div class="bouton">
+        <a class = a2 href="../VUE/Accueil_admin.php">Accueil</a>
+        <a class = a2 href="../VUE/Rechercher_un_client.php">Rechercher un client</a>
+        <a class = a2 href="../VUE/stock.php">Stock capteurs</a>
+        <a class = a2 href="../VUE/new_capteur.php">Nouveau capteur</a>
+        <a class = a2 href=".php">Messagerie</a>
+    </div>
+
+</main>
+
+<?php include '../VUE/Footer.php';?>
+
+</body>
+
+</html>
+
