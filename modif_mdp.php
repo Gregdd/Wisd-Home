57e353ea05c4e97eaa62d806ae70f906d2f5de77
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-06-08 11:15:23 +0200

    Update modif_mdp.php

--- modif_mdp.php
+++ modif_mdp.php
@@ -9,20 +9,22 @@
 
 </head>
 <body>
-<header><?php include 'Header_2.php';?></header>
-<h1>Mot de passe</h1>
+<header>
+ <?php include 'Header_2.php';?></header>
+<?php include('Access_denied.php');?>
+ 
+ <h1>Mot de passe</h1>
 <form method="post" action="mdp_actu.php">
  <div class="Colonne2">
 <fieldset>
+ 
 <legend>Informations sur votre compte</legend>
 <p> <span class="soustitre">Modifier mot de passe : </span><br/><br/>
 <label for="nmdp"> Nouveau mot de passe : </label>
 <input type="password" name="nmdp" id="nmdp" placeholder="*******" size="25" minlength="8"/> <br /><br />
 <label for="cmdp"> Confirmer mot de passe : </label>
 <input type="password" name="cmdp" id="cmdp" placeholder="*******" size="25" minlength="8" required/> <br /><br />
-<input type="submit" value="Valider"/><br/><br />
-
-</p>
+<input type="submit" value="Valider"/><br/><br /></p>
 </fieldset>
 </div>
 </form>
