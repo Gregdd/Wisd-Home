1b7f1a58334f26ef805477e73331eda2fd3cf4c3
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-06-08 14:54:36 +0200

    Update and rename Rechercher_un_client.php to VUE/Rechercher_un_client.php

--- VUE/Rechercher_un_client.php
+++ VUE/Rechercher_un_client.php
@@ -2,17 +2,17 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="Recherche.css" />
+    <link rel="stylesheet" href="/Wisd-Home-master/VUE/css/Recherche.css" />
 </head>
 
 <body>
-<?php include('Access_denied.php');?>
-<?php include 'Header_admin.php';?>
+
+<?php include 'Header_admin2.php';?>
 
 <main>
 
     <h1>Rechercher un client :</h1>
-    <form action="recherche_post.php" method="post">
+    <form action="/Wisd-Home-master/CONTROLEUR/recherche_post.php" method="post">
         <label for="nom">Nom : </label>
         <input type="text" name="nom"><br><br>
         <label for="pseudo">Identifiant : </label>
