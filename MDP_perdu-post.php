f60bd5b2271cf10db1443bfaea43bcab355ecf8b
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-06-08 09:22:48 +0200

    Update MDP_perdu-post.php

--- MDP_perdu-post.php
+++ MDP_perdu-post.php
@@ -1,19 +1,41 @@
 <?php
 
 include 'database.php';
+session_start();
 $pseudo = $_POST['pseudo'];
-$reponse = $bdd->query('SELECT Reponse FROM utilisateur WHERE pseudo LIKE "$pseudo"');
-$question = $bdd->query('SELECT question FROM utilisateur WHERE pseudo LIKE "$pseudo"');
+if(isset($_POST['pseudo'])){
 
+	$reponse = $bdd->prepare('SELECT Reponse FROM utilisateur WHERE pseudo=?');
+	$reponse->execute(array(
+		$pseudo
+	)
+);
+$reponse=$reponse->fetch();
+$question = $bdd->prepare('SELECT question FROM utilisateur WHERE pseudo=?');
+$question->execute(array(
+	$pseudo
+)
+);
+$question=$question->fetch();
+$question=$question[0];
+}
 
-if(!empty($_POST['reponse']) AND $_POST['reponse']==$pseudo['reponse']){
-			$newmdp = $_POST['nmdp'];
-			$req= $bdd -> prepare('UPDATE utilisateur SET Mot_de_passe =:newmdp WHERE pseudo = :ID');
-			$req->execute(array(
-					'newmdp' => $newmdp,
-					'ID' => $userid
-			));}
-      header ('Location:MDP_perdu2.php');
-
-
+if(!empty($_POST['reponse'])){
+$rep=$_POST['reponse'];
+if($rep==$reponse[0] AND $_POST['pass']==$_POST['confirm_pass']){
+	$newpass = $_POST['pass'];
+	$req= $bdd -> prepare('UPDATE utilisateur SET Mot_de_passe =:newmdp WHERE pseudo = :pseudo');
+	$req->execute(array(
+		'newmdp' => $newpass,
+		'pseudo' => $pseudo
+	));
+	include('Connexion.php');
+}
+else{
+	include('MDP_perdu2.php');
+}
+}
+else{
+	include('MDP_perdu2.php');
+}
 ?>
