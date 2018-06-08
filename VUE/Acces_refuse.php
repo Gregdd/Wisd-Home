66438087f2c8934b02680b83d8399c36a160147b
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-06-08 14:28:19 +0200

    Update and rename Acces_refuse.php to VUE/Acces_refuse.php

---
+++ VUE/Acces_refuse.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="/Wisd-Home-master/VUE/css/Acces_refuse.css" />
+</head>
+
+<body>
+
+<?php include 'Header_1.php';?>
+<div class="refus">
+    <fieldset>
+        <legend>Vous n'êtes pas connecté !</legend>
+        <p>Vous devez être connecté pour pouvoir accéder au site.<p>
+        <p>Pour revenir à l'accueil, cliquez <a href="/Wisd-Home-master/VUE/Accueil_1.php">ici</a><p><br/>
+    </fieldset>
+</div>
+
+<?php include 'Footer.php';?>
+
+</body>
+
+</html>
