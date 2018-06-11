683dd7828a9c126b8b2d7afd618deeee73760093
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-06-11 09:32:49 +0200

    Update Connexion.php

--- VUE/Connexion.php
+++ VUE/Connexion.php
@@ -27,7 +27,7 @@
                 <input type="submit" value="Connexion"><br/><br/>
             </form>
 
-            <a href="CLIENT/Mdp_oublie.php" title="Mot de passe oublié" target="_blank">Mot de passe oublié </a>
+            <a href="Mdp_oublie.php" title="Mot de passe oublié" target="_blank">Mot de passe oublié </a>
 
         </fieldset>
     </div>
