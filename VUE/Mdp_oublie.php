1a59b4d656c552d519a7a4b2e456d3361b02c3fa
mariegourlet <37437867+mariegourlet@users.noreply.github.com>
2018-06-11 09:35:06 +0200

    Update and rename Mdp_oublie.php to VUE/Mdp_oublie.php

--- VUE/Mdp_oublie.php
+++ VUE/Mdp_oublie.php
@@ -4,8 +4,8 @@
 <head>
 <meta charset="utf-8"/>
 <title>Modification du mot de passe</title>
-<link rel="stylesheet" href="Mdp_oublie-post.css"/>
-<script type="text/javascript" src="Mdp_oublie.js"></script>
+<link rel="stylesheet" href="css/Mdp_oublie-post.css"/>
+<script type="text/javascript" src="../CONTROLEUR/Mdp_oublie.js"></script>
 </head>
 
 <body>
@@ -17,7 +17,7 @@
     <fieldset>
       <legend>Modification du mot de passe</legend><br/>
       <p>Nous allons vous envoyer un mail pour que vous puissiez récupérer votre compte en changeant votre mot de passe.</p><br/>
-      <form method="post" action="mdp_post.php">
+      <form method="post" action="../CONTROLEUR/mdp_post.php">
         <label for="mail">Veuillez entrer votre adresse mail :</label><br/><br/>
         <div id="failmail">Adresse mail invalide !</div><br/>
         <input type="text" id="mail" name="mail"/><br/><br/>
