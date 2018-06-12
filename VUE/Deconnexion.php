cf0b6236b852bd2fdfc852c4893b6ec67e255e90
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-06-12 10:07:50 +0200

    Create Deconnexion.php

---
+++ VUE/Deconnexion.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Wisd'Home - Déconnexion</title>
+    <link rel="stylesheet" href="/Wisd-Home-master/VUE/css/Acces_refuse.css"/>
+</head>
+
+<body>
+
+<?php include '../VUE/Header_1.php'; ?>
+
+<fieldset>
+<p>Vous êtes à présent déconnecté</p>
+Pour revenir a l'accueil, cliquez <a href="../VUE/Accueil_1.php">ici</a>
+</fieldset>
+
+<?php include '../VUE/Footer.php';?>
+
+</body>
+
+</html>
