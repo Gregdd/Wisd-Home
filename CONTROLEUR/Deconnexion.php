cb7a9538da487972f6459fcf39aabf954374590d
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-06-12 10:08:21 +0200

    Update Deconnexion.php

--- CONTROLEUR/Deconnexion.php
+++ CONTROLEUR/Deconnexion.php
@@ -5,5 +5,5 @@ if (isset ($_SESSION['pseudo']))
     setcookie('pseudo', '', -1);
 }
 session_destroy();
-    echo 'Vous êtes à présent déconnecté <br/> Pour revenir a l\'accueil, cliquez <a href="../VUE/Accueil_1.php">ici</a>';
+header ('Location: ../VUE/Deconnexion.php');
 ?>
