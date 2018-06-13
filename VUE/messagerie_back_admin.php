3cf00fc8ce6fb03bed27dee233174ef0bfcde129
salimghaiti <34586457+salimghaiti@users.noreply.github.com>
2018-06-13 11:14:51 +0200

    Update messagerie_back_admin.php

--- VUE/messagerie_back_admin.php
+++ VUE/messagerie_back_admin.php
@@ -66,7 +66,7 @@ include '../CONTROLEUR/database.php';
 <br>
 <center>
     Supprimer une question
-    <form action="../MODELE/del_question.php" method="post">
+    <form action="../MODELE/fonctionSQL.php" method="post">
         <div class="form">
             <label><input type="number" name="ID" placeholder="ID"></label><br></span>
             <input type="submit" value="Supprimer"></div>
