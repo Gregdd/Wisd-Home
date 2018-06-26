d41aee417b1931855655f76eab51be755613ca07
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-06-26 11:00:48 +0200

    Update new_capteur.php

--- VUE/new_capteur.php
+++ VUE/new_capteur.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="/Wisd-Home-master/VUE/css/Recherche.css" />
+    <link rel="stylesheet" href="/Wisd-Home-master/VUE/css/new_capteur.css" />
 </head>
 
 <body>
