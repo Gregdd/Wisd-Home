f1047c24cf430ca57b98249805dd06ef16f2ab9e
Marvin-Nylce <32478799+Marvin-Nylce@users.noreply.github.com>
2018-06-27 10:21:02 +0200

    Create CGU.php

---
+++ VUE/CGU.php
@@ -0,0 +1,110 @@
+<html>
+
+<head>
+	
+    <meta charset="utf-8" />
+    <title>CGU</title>
+    <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+    <script src="bootstrap-3.1.1-dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+<h1>Mentions légales</h1>
+<p>Le site Wisdhome.fr est édité par :<br/>
+SARL Pi'Group <br/>
+28 Rue Notre Dame des Champs <br/>
+75006 Paris <br/>
+</p>
+<p>
+SARL au capital de 00,00 euros <br/>
+RCS Paris 000 000 000<br/> 
+N° de TVA intracommunautaire : FR 10 98 76 54 32<br/>
+Le directeur de la publication est Sylvain Lefebvre. <br/>
+</p>
+<p>
+CCM Benchmark Group est une société du groupe Howto Media Group<br/> 
+Société Anonyme au capital de 1 893 212,05 euros, <br/>
+94, rue de Provence 75009 Paris <br/>
+RCS Paris XXX XXX XX XX <br/><br/>
+Le site Wisdhome.com est hébergé par : <br/><br/><br/>
+ISEP <br/>
+28 rue Notre Dame des Champs <br/>
+75006 Paris <br/>
+</p>
+<br/>
+<br/>
+<h2>Définitions</h2>
+<p><em><strong>Utilisateur</strong></em> : L'Utilisateur est toute personne qui utilise le Site ou l'un des services proposés sur le Site.
+Contenu Utilisateur : Le terme « Contenu Utilisateur » désigne les données transmises par l'Utilisateur dans les différentes rubriques du Site.<br/>
+<em><strong>Membre</strong></em> : Le terme « Membre » désigne un utilisateur identifié sur le site.<br/>
+<strong><em>Identifiant</strong></em> : Le terme « Identifiant » recouvre les informations nécessaires à l'identification d'un utilisateur sur le site pour accéder aux zones réservées aux membres.
+Mot de passe : Le « Mot de passe » est une information confidentielle, dont l'Utilisateur doit garder le secret, lui permettant, utilisé conjointement avec son Identifiant, de prouver l'identité.
+</p>
+
+<p>Le Service est accessible gratuitement à tout Utilisateur disposant d'un accès à internet. Tous les coûts afférents à l'accès au Service, que ce soient les frais matériels, logiciels ou d'accès à internet sont exclusivement à la charge de l'utilisateur. Il est seul responsable du bon fonctionnement de son équipement informatique ainsi que de son accès à internet. 
+Certaines sections du site sont réservées aux Membres après identification à l'aide de leur Identifiant et de leur Mot de passe.<br/><br/>
+
+Wisdhome se réserve le droit de refuser l'accès au Service, unilatéralement et sans notification préalable, à tout Utilisateur ne respectant pas les présentes conditions d'utilisation. <br/><br/>
+
+Pi'Group met en oeuvre tous les moyens raisonnables à sa disposition pour assurer un accès de qualité au Service, mais n'est tenu à aucune obligation d'y parvenir. <br/><br/>
+
+Pi'Group ne peut, en outre, être tenue responsable de tout dysfonctionnement du réseau ou des serveurs ou de tout autre événement échappant au contrôle raisonnable, qui empêcherait ou dégraderait l'accès au Service. <br/><br/>
+
+Pi'Group se réserve la possibilité d'interrompre, de suspendre momentanément ou de modifier sans préavis<br/> 
+l'accès à tout ou partie du Service, afin d'en assurer la maintenance, ou pour toute autre raison, sans que l'interruption n'ouvre droit à aucune obligation ni indemnisation.<br/> 
+</p><br/><br/>
+<h2>Accès au service</h2><br/>
+
+<p>
+Le Service est accessible gratuitement à tout Utilisateur disposant d'un accès à internet. Tous les coûts afférents à l'accès au Service, que ce soient les frais matériels, logiciels ou d'accès à internet sont exclusivement à la charge de l'utilisateur. Il est seul responsable du bon fonctionnement de son équipement informatique ainsi que de son accès à internet. 
+Certaines sections du site sont réservées aux Membres après identification à l'aide de leur Identifiant et de leur Mot de passe. <br/><br/>
+
+Wisdhome se réserve le droit de refuser l'accès au Service, unilatéralement et sans notification préalable, à tout Utilisateur ne respectant pas les présentes conditions d'utilisation. <br/><br/>
+
+Pi'Group met en oeuvre tous les moyens raisonnables à sa disposition pour assurer un accès de qualité au Service, mais n'est tenu à aucune obligation d'y parvenir. <br/><br/>
+
+Pi'Group ne peut, en outre, être tenue responsable de tout dysfonctionnement du réseau ou des serveurs ou de tout autre événement échappant au contrôle raisonnable, qui empêcherait ou dégraderait l'accès au Service. <br/><br/>
+
+Pi'Group se réserve la possibilité d'interrompre, de suspendre momentanément ou de modifier sans préavis <br/>
+l'accès à tout ou partie du Service, afin d'en assurer la maintenance, ou pour toute autre raison, sans que l'interruption n'ouvre droit à aucune obligation ni indemnisation. </p>
+<h2>Propriété intellectuelle</h2><br/><br/>
+Les textes et illustrations dont la mention le précise sont soumis à la licence Creative Commons et peuvent sous certaines conditions être reproduits, distribués ou modifiés et ce, sans nécessairement en demander l'autorisation. <br/><br/>
+
+« Wisdhome » et « Pi'Group » sont des marques déposées de Pi'Group. Toute reproduction non autorisée de ces marques, logos et signes distinctifs constitue une contrefaçon passible de sanctions pénales. Le contrevenant s'expose à des sanctions civiles et pénales et notamment aux peines prévues aux articles L. 335.2 et L. 343.1 du code de la Propriété Intellectuelle. <br/><br/>
+
+L'Utilisateur est seul responsable du Contenu Utilisateur qu'il met en ligne via le Service, ainsi que des textes et/ou opinions qu'il formule. Il s'engage notamment à ce que ces données ne soient pas de nature à porter atteinte aux intérêts légitimes de tiers quels qu'ils soient. A ce titre, il garantit Pi'Group contre tous recours, fondés directement ou indirectement sur ces propos et/ou données, susceptibles d'être intentés par quiconque à l'encontre de Pi'Group. Il s'engage en particulier à prendre en charge le paiement des sommes, quelles qu'elles soient, résultant du recours d'un tiers à l'encontre de Pi'Group, y compris les honoraires d'avocat et frais de justice.<br/><br/> 
+
+Pi'Group se réserve le droit de supprimer tout ou partie du Contenu Utilisateur, à tout moment et pour quelque raison que ce soit, sans avertissement ou justification préalable. L'Utilisateur ne pourra faire valoir aucune réclamation à ce titre. <br/><br/></p>
+<h2>Limites de responsabilité</h2>
+<p>
+Le site Wisdhome.com est un site permettant la gestion de domiciles. <br/>
+Les informations diffusées sur le site Wisdhome.com proviennent de sources réputées fiables. Toutefois, Pi'Group ne peut garantir l'exactitude ou la pertinence de ces données. En outre, les informations mises à disposition sur ce site le sont uniquement à titre purement informatif et ne sauraient constituer en aucun cas un conseil ou une recommandation de quelque nature que ce soit. <br/><br/>
+
+En conséquence, l'Utilisation des informations et contenus disponibles sur l'ensemble du site, ne sauraient en aucun cas engager la responsabilité de Pi'Group, à quelque titre que ce soit. L'Utilisateur est seul maître de la bonne utilisation, avec discernement et esprit, des informations mises à sa disposition sur le Site. <br/><br/>
+
+Par ailleurs, l'Utilisateur s'engage à indemniser Pi'Group de toutes conséquences dommageables liées directement ou indirectement à l'usage qu'il fait du Service. <br/><br/>
+
+L'accès à certaines sections du site Wisdhome.fr nécessite l'utilisation d'un Identifiant et d'un Mot de passe. Le Mot de passe, choisi par l'utilisateur, est personnel et confidentiel. L'utilisateur s'engage à conserver secret son mot de passe et à ne pas le divulguer sous quelque forme que ce soit. L'utilisation de son Identifiant et de son Mot de passe à travers internet se fait aux risques et périls de l'Utilisateur. Il appartient à l'Utilisateur de prendre toutes les dispositions nécessaires permettant de protéger ses propres données contre toute atteinte. <br/><br/>
+
+Pi'Group s'engage néanmoins à mettre en place tous les moyens nécessaires pour garantir la sécurité et la confidentialité des données transmises. L'Utilisateur est informé qu'un ou plusieurs cookies, ne contenant aucune information personnelle, pourront être placés sur son disque dur afin d'assurer son identification.<br/><br/> 
+
+L'Utilisateur admet connaître les limitations et contraintes propres au réseau internet et, à ce titre, reconnaît notamment l'impossibilité d'une garantie totale de la sécurisation des échanges de données. Pi'Group ne pourra pas être tenue responsable des préjudices découlant de la transmission de toute information, y compris de celle de son identifiant et/ou de son mot de passe, via le Service.<br/><br/>
+
+Pi'Group ne pourra en aucun cas, dans la limite du droit applicable, être tenue responsable des dommages et/ou préjudices, directs ou indirects, matériels ou immatériels, ou de quelque nature que ce soit, résultant d'une indisponibilité du Service ou de toute Utilisation du Service. Le terme « Utilisation » doit être entendu au sens large, c'est-à-dire tout usage du site quel qu'il soit, licite ou non. <br/><br/>
+
+L'Utilisateur s'engage, d'une manière générale, à respecter l'ensemble de la réglementation en vigueur en France. </p><br/><br/>
+
+<h2>Liens hypertextes</h2>
+<p>
+Wisdhome.com propose des liens hypertextes vers des sites web édités et/ou gérés par des tiers. <br/>
+Dans la mesure où aucun contrôle n'est exercé sur ces ressources externes, l'Utilisateur reconnaît que Pi'Group n'assume aucune responsabilité relative à la mise à disposition de ces ressources, et ne peut être tenue responsable quant à leur contenu. <br/>
+<h2>Force majeure</h2><br/>
+La responsabilité de Pi'Group ne pourra être engagée en cas de force majeure ou de faits indépendants de sa volonté. <br/>
+<br/><h2>Evolution du présent contrat</h2>
+<br/><p>Pi'Group se réserve le droit de modifier les termes, conditions et mentions du présent contrat à tout moment. <br/>
+Il est ainsi conseillé à l'Utilisateur de consulter régulièrement la dernière version des Conditions d'Utilisation disponible sur le site www.Wisdhome.fr. </p>
+<h2>Durée et résiliation</h2><br/>
+<p>Le présent contrat est conclu pour une durée indéterminée à compter de l'Utilisation du Service par l'Utilisateur.</p>
+<h2>Droit applicable et juridiction compétente</h2><br/>
+<p>Les règles en matière de droit, applicables aux contenus et aux transmissions de données sur et autour du site, sont déterminées par la loi française. En cas de litige, n'ayant pu faire l'objet d'un accord à l'amiable, seuls les tribunaux français du ressort de la cour d'appel de Paris sont compétents. </p>
+</body>
+</html>
