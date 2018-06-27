ae711e1ff859de76f133eff70f4c0d8203bd7c8f
Gregdd <37437720+Gregdd@users.noreply.github.com>
2018-06-27 09:18:40 +0200

    Update inscription_post2.php

--- CONTROLEUR/inscription_post2.php
+++ CONTROLEUR/inscription_post2.php
@@ -22,12 +22,10 @@ $reponse = $_POST['reponse'];
 
 $bday = $_POST['bday'];
 
-$salt = 'WisdHome';
-$pass_hache = sha1($_POST['identifiant'] . hash('sha256', $salt) . $_POST['pass']);
 
 if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
 
-    inscription($pseudo,$pass_hache,$nom,$prenom,$mail,$adresse,$ville,$code_post,$bday,$reponse,$question);
+    inscription($pseudo,$pass,$nom,$prenom,$mail,$adresse,$ville,$code_post,$bday,$reponse,$question);
 
     header('Location: ../VUE/Connexion.php');
 }
